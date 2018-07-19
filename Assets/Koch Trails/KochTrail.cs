01294865207885a6a2d1abda6fd1b59d54c4058c
Jesús Pérez <jesus-perez95@hotmail.com>
2018-07-19 11:40:42 -0400

    [Pol] Trail render behaviour base

--- Assets/Koch Trails/KochTrail.cs
+++ Assets/Koch Trails/KochTrail.cs
@@ -12,7 +12,6 @@ public class KochTrail : KochGenerator {
         public int CurrentTargetNum { get; set; }
         public Vector3 TargetPosition { get; set; }
         public Color EmissionColor { get; set; }
-
     }
 
     [HideInInspector]
@@ -30,6 +29,13 @@ public class KochTrail : KochGenerator {
     [Header("Audio")]
     [SerializeField] private AudioPeer audioPeer;
     public int[] audioBand;
+    public Vector2 speedMinMax;
+
+
+    //Private variales
+    private float lerpPosSpeed;
+    private float distanceSnap;
+
 
 	// Use this for initialization
 	void Start () {
@@ -43,6 +49,7 @@ public class KochTrail : KochGenerator {
             trailObjectsInstance.Trail.material = new Material(trailMaterial);
             trailObjectsInstance.EmissionColor = trailColor.Evaluate(i * (1.0f / initiatorPointAmount));
             trailObjectsInstance.Trail.numCapVertices = trailEndCapVertices;
+            trailObjectsInstance.Trail.widthCurve = trailWidthCurve;
 
             Vector3 instantiatePosition;
             if (generationCount > 0)
@@ -53,6 +60,7 @@ public class KochTrail : KochGenerator {
                     step = bezierPositions.Length / initiatorPointAmount;
                     instantiatePosition = bezierPositions[i * step];
                     trailObjectsInstance.CurrentTargetNum = (i * step) + 1;
+                    trailObjectsInstance.TargetPosition = bezierPositions[trailObjectsInstance.CurrentTargetNum];
 
                 } 
                 else
@@ -60,6 +68,7 @@ public class KochTrail : KochGenerator {
                     step = positions.Length / initiatorPointAmount;
                     instantiatePosition = positions[i * step];
                     trailObjectsInstance.CurrentTargetNum = (i * step) + 1;
+                    trailObjectsInstance.TargetPosition = positions[trailObjectsInstance.CurrentTargetNum];
                 }
             }
             else
@@ -73,9 +82,49 @@ public class KochTrail : KochGenerator {
             trails.Add(trailObjectsInstance);
         }
 	}
+
+    private void Movement()
+    {
+        lerpPosSpeed = Mathf.Lerp(speedMinMax.x, speedMinMax.y, 100f);
+        for (int i = 0; i < trails.Count; i++)
+        {
+            trails[i].GO.transform.localPosition = Vector3.MoveTowards(trails[i].GO.transform.localPosition, trails[i].TargetPosition, Time.deltaTime * lerpPosSpeed);
+            distanceSnap = Vector3.Distance(trails[i].GO.transform.localPosition, trails[i].TargetPosition);
+
+            if (distanceSnap < 0.05f)
+            {
+                trails[i].GO.transform.localPosition = trails[i].TargetPosition;
+                if (isUsingBezierCurves && generationCount > 0)
+                {
+                    if (trails[i].CurrentTargetNum < bezierPositions.Length - 1)
+                    {
+                        trails[i].CurrentTargetNum += 1;
+                    }
+                    else
+                    {
+                        trails[i].CurrentTargetNum = 1;
+                    }
+                    trails[i].TargetPosition = bezierPositions[trails[i].CurrentTargetNum];
+                }
+                else
+                {
+                    if (trails[i].CurrentTargetNum < positions.Length - 1)
+                    {
+                        trails[i].CurrentTargetNum += 1;
+                    }
+                    else
+                    {
+                        trails[i].CurrentTargetNum = 1;
+                    }
+                    trails[i].TargetPosition = targetPositions[trails[i].CurrentTargetNum];
+                }
+            }
+        }
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        Movement();
 	}
 }
